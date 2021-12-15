c89be3ba3ce9c9d1a689e2782cad75f3cadce6d3
AndreiADASKA <78017302+AndreiADASKA@users.noreply.github.com>
2021-12-15 19:54:03 +0300

    hm formik form

---
+++ formformik/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Formikkk from './Formikkk';
+
+export default class App extends Component {
+  render() {
+    return <Formikkk />;
+  }
+}
---
+++ formformik/src/Formikkk.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Formik, Form, Field } from 'formik';
+
+export default class Formikkk extends Component {
+  render() {
+    return (
+      <Formik
+        initialValues={{
+          username: '',
+          adults: '',
+          children: '',
+          dateIn: '',
+          dateOut: '',
+          smoking: 'некурящий',
+          prepayment: false,
+        }}
+        onSubmit={(formValues) => alert(JSON.stringify(formValues))}
+      >
+        <Form>
+          <div className="form">
+            {/* {' '}  При формате prettier появляются эти скобки */}
+            <div className="name-form">
+              <label>Name</label>
+              <Field name="username" />
+            </div>
+            <div className="number">
+              <div>
+                <label htmlFor="">Количество взрослых</label>
+                <Field as="select" name="adults">
+                  <option value="1">один</option>
+                  <option value="2">два</option>
+                  <option value="от 2х до 5">от двух до пяти</option>
+                  <option value="5 и более">пять и более</option>
+                </Field>
+              </div>
+              <div>
+                <label htmlFor="">Количество детей</label>
+                <Field as="select" name="children">
+                  <option value="1">один</option>
+                  <option value="2">два</option>
+                  <option value="от 3х до 5">от трех до пяти</option>
+                  <option value="5 и более">пять и более</option>
+                </Field>
+              </div>
+            </div>
+
+            <div className="date">
+              <div>
+                <label htmlFor="">Прибытие</label>
+                <Field type="date" name="dateIn"></Field>
+              </div>
+              <div>
+                <label htmlFor="">Выезд</label>
+                <Field type="date" name="dateOut"></Field>
+              </div>
+            </div>
+
+            <div className="smoke">
+              <label>
+                <Field type="radio" name="smoking" value="курящий" />
+                Номер для курящих
+              </label>
+              <label>
+                <Field type="radio" name="smoking" value="некурящий" />
+                Номер для некурящих
+              </label>
+            </div>
+            <div className="prepayment">
+              <label>
+                <Field type="checkbox" name="prepayment" />Я гарантирую предоплату
+              </label>
+            </div>
+            <button type="submit">Отправить</button>
+          </div>
+        </Form>
+      </Formik>
+    );
+  }
+}
